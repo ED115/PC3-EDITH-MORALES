b4c297b92986a1b872361eb5bf149ed95d251988
Edith <Edith@LAPTOP-46IGGMRT>
2020-07-02 00:09:55 -0500

    commit PC03

---
+++ src/main/java/com/redsocial/Pc03EntrenadorEquipoApplication.java
@@ -0,0 +1,13 @@
+package com.redsocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pc03EntrenadorEquipoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pc03EntrenadorEquipoApplication.class, args);
+	}
+
+}
